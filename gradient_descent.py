250228424a7fdf615da1f71db18dbc1695756b32
gs25031-cell <gs25031@gs.hs.kr>
2026-01-13 13:04:27 +0900

    Update gradient_descent.py

--- gradient_descent.py
+++ gradient_descent.py
@@ -29,7 +29,7 @@ def gradient_descent(starting_values: tuple, learning_rate: float, num_iteration
     # Your code here
     a,b=starting_values
     for _ in range(num_iterations):
-        dx, dy= gradient_f(x,y)
-        x=x-learning_rate*dx
-        y=y-learning_rate*dy
-    return (x,y)
+        da, db= gradient_f(a,b)
+        a=a-learning_rate*da
+        b=b-learning_rate*db
+    return (a,b)
